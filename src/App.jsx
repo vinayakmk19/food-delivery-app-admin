0876904c285af39caf71f4aeb5a53b218f081508
vinayakmk19 <vinayakmkgen@gmail.com>
2024-05-10 16:49:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
