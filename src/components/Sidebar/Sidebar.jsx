44f02ebda29f60ba8d5732a7a30371b865d5b9d7
vinayakmk19 <vinayakmkgen@gmail.com>
2024-05-20 19:14:20 +0530

    pages folders and syntax are created

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
-        <div className="sidebar-option">
+        <NavLink to='/add' className="sidebar-option">
           <img src={assets.add_icon} alt="" />
           <p>Add items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>List items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   )
---
+++ src/pages/Orders/Orders.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Orders.css'
+
+
+const Orders = () => {
+  return (
+    <div>
+      Orders
+    </div>
+  )
+}
+
+export default Orders
